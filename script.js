cb057744d983aee243a8cec1c466b281045f1ac7
David Stobbe <d.stobbe1@share.epsb.ca>
2022-02-15 13:00:01 -0700

    Done validation

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
 // RGB colors with JS by David S.
-
+// Variables to store HTML Elements
+let redInEl = document.getElementById("Red");
+let greenInEl = document.getElementById("Green");
+let blueInEl = document.getElementById("Blue");
+let rgbStringOutEl = document.getElementById("rgbStrOut");
+let displayEl = document.getElementById("Display");
 // Event Listener
-
-document.getElementById("preview-btn").addEventListener("click", rgbPreview);
+redInEl.addEventListener("input", rgbPreview)
+greenInEl.addEventListener("input", rgbPreview)
+blueInEl.addEventListener("input", rgbPreview)
 
 function rgbPreview() {
     // Input: get red green bluen values.
-    let rValue = document.getElementById("Red").value;
-    let gValue = document.getElementById("Green").value;
-    let bValue = document.getElementById("Blue").value;
+    let rValue = +redInEl.value;
+    let gValue = +greenInEl.value;
+    let bValue = +blueInEl.value;
 
     
-
+    // Validate Colors (Constrain colors between 0 - 255)
+    // Check rValue
+    if (rValue < 0) {
+        rValue = 0;
+        redInEl.value = 0;
+    } else if (rValue > 255) {
+        rValue = 255;
+        redInEl.value = 255;
+    }
+    // Check gValue
+    if (gValue < 0) {
+        gValue = 0;
+        greenInEl.value = 0;
+    } else if (gValue > 255) {
+        gValue = 255;
+        greenInEl.value = 255;
+    }
+    // Check bValue
+    if (bValue < 0) {
+        bValue = 0;
+        blueInEl.value = 0;
+    } else if (bValue > 255) {
+        bValue = 255;
+        blueInEl.value = 255;
+    }
 
 
     // Preocess: build rgb string(__, __, __)
     let rgbString = "rgb(" + rValue + "," + gValue + "," + bValue + ")";
 
     // Output: display rgb string and update the color preview.
-    document.getElementById("rgbStrOut").innerHTML = rgbString
-    document.getElementById("Display").style.background = rgbString;
+    rgbStringOutEl.innerHTML = rgbString
+    displayEl.style.background = rgbString;
 
 }
